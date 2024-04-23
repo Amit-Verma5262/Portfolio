71d2b3cbe5d492b884322709db592d584fc40393
Amit-Verma5262 <amitverma5262.02@gmail.com>
2024-04-23 18:57:32 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var myButton = document.getElementById('btn1');
+
+myButton.addEventListener('click', function() {
+ 
+  alert('Button clicked!');
+});
+var searchButton = document.getElementById('searchbtn');
+
+searchButton.addEventListener('click', function() {
+ 
+  alert('Please wait! Searching');
+});
+
+
+
