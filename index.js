25f39b6eb657d4f77cbecfc05a84dd590b279657
Amit-Verma5262 <amitverma5262.02@gmail.com>
2024-04-23 20:52:18 +0530

    Third commit

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-var myButton = document.getElementById('btn1');
+// Submit Button Click Event Listener
+var submitButton = document.getElementById('btn1');
 
-myButton.addEventListener('click', function() {
- 
-  alert('Button clicked!');
-});
+if (submitButton) {
+  submitButton.addEventListener('click', function() {
+    alert('Thanks for visiting! Form Submitted');
+  });
+} else {
+  console.error('Submit button not found.');
+}
+
+// Search Button Click Event Listener
 var searchButton = document.getElementById('searchbtn');
 
-searchButton.addEventListener('click', function() {
- 
-  alert('Please wait! Searching');
+if (searchButton) {
+  searchButton.addEventListener('click', function() {
+    alert('Please wait! Searching');
+  });
+} else {
+  console.error('Search button not found.');
+}
+
+// JavaScript for Typing Animation
+const typedHeading = document.getElementById('typed-heading');
+const headingText = 'Amit Verma';
+let charIndex = 0;
+
+function typeHeading() {
+  if (charIndex < headingText.length) {
+    typedHeading.textContent += headingText[charIndex];
+    charIndex++;
+    setTimeout(typeHeading, 100); // Adjust the typing speed if needed
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(typeHeading, 2000); // Start typing after 2 seconds (adjust as needed)
 });
-
-
-
